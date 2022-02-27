753604cc83befb130ff020e3044c34e82e568107
Mahin077 <mmahinm10@gmail.com>
2022-02-27 17:45:29 +0600

    Initial commit

---
+++ carmax/Controllers/AdminController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using carmax.Models;
+using System.IO;
+using System.Net;
+using System.Data.Entity;
+
+namespace carmax.Controllers
+{
+    public class AdminController : Controller
+    {
+        carmaxEntities1 db = new carmaxEntities1();
+        
+        public ActionResult Index()
+        {
+            if (Session["userType"] != null && Session["userType"].Equals("admin"))
+            {
+                return View("allCars",db.products.ToList());
+            }
+            return Content("Please log in as Admin to use the url!");
+        }
+        public ActionResult Create()
+        {
+            if (Session["userType"] != null && Session["userType"].Equals("admin"))
+            {
+                return View();
+            }
+            return Content("Please log in as Admin to use the url!");   
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Price,Max_power,Seating_capacity,Body_type,Fuel_type,No_of_cylinder,Color,Engine_type,Engine_displacement,Type,Image,Brand,Model,Year,Discount,File")] product car)
+        {
+            if (ModelState.IsValid)
+            {
+                var filename = Path.GetFileName(car.File.FileName);
+                string _filename = DateTime.Now.ToString("hhmmssfff") + filename;
+                string path = Path.Combine(Server.MapPath("~/Images/"), _filename);
+                car.img = "~/Images/" + _filename;
+
+                db.products.Add(car);
+                if (db.SaveChanges() > 0)
+                {
+                    car.File.SaveAs(path);
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(car);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (Session["userType"] != null && Session["userType"].Equals("admin"))
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                product car = db.products.Find(id);
+                Session["imgPath"] = car.img;
+                if (car == null)
+                {
+                    return HttpNotFound();
+                }
+                return View("Edit", car);
+            }
+            return Content("Please log in as Admin to use the url!");
+
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Price,Max_power,Seating_capacity,Body_type,Fuel_type,No_of_cylinder,Color,Engine_type,Engine_displacement,Type,Image,Brand,Model,Year,Discount, File")] product car)
+        {
+            if (ModelState.IsValid)
+            {
+                if (car.File != null)
+                {
+                    var filename = Path.GetFileName(car.File.FileName);
+                    string _filename = DateTime.Now.ToString("hhmmssfff") + filename;
+                    string path = Path.Combine(Server.MapPath("~/Images/"), _filename);
+                    car.img = "~/Images/" + _filename;
+
+                    db.Entry(car).State = EntityState.Modified;
+                    string oldImgPath = Request.MapPath(Session["imgPath"].ToString());
+                    if (db.SaveChanges() > 0)
+                    {
+                        car.File.SaveAs(path);
+                        if (System.IO.File.Exists(oldImgPath))
+                        {
+                            System.IO.File.Delete(oldImgPath);
+                        }
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    car.img = Session["imgPath"].ToString();
+
+                    db.Entry(car).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View("Edit",car);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (Session["userType"] != null && Session["userType"].Equals("admin"))
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                product car = db.products.Find(id);
+                if (car == null)
+                {
+                    return HttpNotFound();
+                }
+                return View("Delete",car);
+            }
+            return Content("Please log in as Admin to use the url!");
+            
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product car = db.products.Find(id);
+            string currentImg = Request.MapPath(car.img);
+            db.products.Remove(car);
+            if (db.SaveChanges() > 0)
+            {
+                if (System.IO.File.Exists(currentImg))
+                {
+                    System.IO.File.Delete(currentImg);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ carmax/Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using carmax.Models;
+namespace carmax.Controllers
+{
+    public class HomeController : Controller
+    {
+        carmaxEntities1 db = new carmaxEntities1();
+        public ActionResult Index()
+        {
+            
+            List<product> car = db.products.Take(3).ToList();
+
+            return View(car);
+        }
+        public ActionResult Register()
+        {
+            if(Session["username"] == null)
+            {
+                return View("Register");
+            }
+            else
+            {
+                return Content("The url does not exist");
+            }
+        }
+        [HttpPost]
+        public ActionResult RegisterData(string username,string email,string phone,string password)
+        {
+            int resCount = db.logins.Where(temp => temp.email.Equals(email)).Count();
+            if(resCount>0)
+            {
+               
+                TempData["msg"] = "The email already exist! Please log in.";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                System.Security.Cryptography.MD5CryptoServiceProvider test123 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(password);
+                data = test123.ComputeHash(data);
+                String newPassword = System.Text.Encoding.ASCII.GetString(data);
+                login lg = new login();
+                lg.username = username;
+                lg.email = email;
+                lg.phone = phone;
+                lg.type = "normal";
+                lg.password = newPassword;
+                db.logins.Add(lg);
+                db.SaveChanges();
+
+               
+                TempData["msg"] = "Register completed! Please log in.";
+                return RedirectToAction("Login");
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult LoginData(string email,string password)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider test123 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(password);
+            data = test123.ComputeHash(data);
+            String newPassword = System.Text.Encoding.ASCII.GetString(data);
+            int resCount = db.logins.Where(temp => temp.email.Equals(email) & temp.password.Equals(newPassword)).Count();
+            if(resCount > 0)
+            {
+                List<login> user = db.logins.Where(temp=> temp.email.Equals(email) ).ToList();
+                foreach (var i in user)
+                {
+                    Session["username"] = i.username;
+                    Session["userType"] = i.type;
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                
+                TempData["msg"] = "Email or Password does not match! Try again.";
+                return RedirectToAction("Login");
+            }
+        }
+        public ActionResult Login()
+        {
+
+            if (Session["username"] == null)
+            {
+                if(TempData["msg"]!=null)
+                {
+                    ViewBag.Message = TempData["msg"].ToString();
+                }
+                return View("Login");
+            }
+            else
+            {
+                return Content("The url does not exist");
+            }
+        }
+
+        public ActionResult Cars()
+        {
+            List<product> car = db.products.Where(temp => temp.type.Equals("normal")).ToList();
+            return View(car);
+        }
+        public ActionResult SpecialOffers()
+        {
+            if(Session["username"] != null)
+            {
+                List<product> car = db.products.Where(temp => temp.type.Equals("private")).ToList();
+                return View(car);
+            }
+            else
+            {
+                TempData["msg"] = "Please login to see this page";
+                return RedirectToAction("Login");
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
