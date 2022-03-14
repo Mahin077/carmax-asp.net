1c4b90b70bfcfd99e37015ae188806c3455be839
Faysal Mahmud <faysalmahmud735@gmail.com>
2022-03-14 12:27:59 +0600

    database

---
+++ database/carmax.sql
@@ -0,0 +1,8 @@
+select * from teammembers
+update teammembers set images = '~/Images/Faysal.jpg' where id = 1
+update teammembers set images = '~/Images/MahiN.png' where id = 2
+update teammembers set images = '~/Images/akib.jpeg' where id = 3
+
+select * from order_cars
+
+delete from order_cars where id = 6
